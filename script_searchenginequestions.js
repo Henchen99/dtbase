0d61008a274f41e3bcf5ee90df926cdfb6acc86c
Will12Stevo <willstevens2000@hotmail.co.uk>
2024-02-18 21:54:42 +0000

    Bosh

--- script_searchenginequestions.js
+++ script_searchenginequestions.js
@@ -497,3 +497,113 @@ document.addEventListener('keydown', function(event) {
         }
     }
 });
+
+
+
+
+
+
+
+
+// const hamburgers = document.getElementsByClassName('hamburger')[0];
+// const nav_link_wrapper = document.getElementsByName('nav-link-wrapper')[0];
+
+// hamburgers.addEventListener('click', () => {
+//     nav_link_wrapper.classList.toggle('active');
+// })
+
+
+// const toggleButton = document.getElementsByClassName('hamburger')[0]
+// const navbarLinks = document.getElementsByClassName('nav-link-wrapper')[0]
+
+// toggleButton.addEventListener('click', () => {
+//   navbarLinks.classList.toggle('active')
+// })
+
+
+const toggleButton = document.getElementsByClassName('hamburger')[0];
+const navbarLinks = document.querySelectorAll('.nav-link-wrapper');
+
+toggleButton.addEventListener('click', () => {
+    toggleButton.classList.toggle('active');
+    navbarLinks.forEach((it) => {
+        it.classList.toggle('active');
+    });
+
+});
+
+// const toggleArrow = document.getElementsByClassName('drop_down')[0];
+// const courseworkDrop = document.querySelectorAll('.portfolio-item-wrapper-notes-coursework');
+
+// toggleArrow.addEventListener('click', () => {
+//     console.log("Clicked");
+//     toggleArrow.classList.toggle('active');
+//     courseworkDrop.forEach((it) => {
+//         it.classList.toggle('active');
+//     });
+// });
+
+// const toggleArrow_pastpaper = document.getElementsByClassName('drop_down_pp')[0];
+// const pastpaperDrop = document.querySelectorAll('.portfolio-item-wrapper-notes-pastpaper');
+
+// toggleArrow_pastpaper.addEventListener('click', () => {
+//     console.log("Clicked");
+//     toggleArrow_pastpaper.classList.toggle('active');
+//     pastpaperDrop.forEach((it) => {
+//         it.classList.toggle('active');
+//     });
+// });
+
+
+// const toggleArrowsPastpaper = document.querySelectorAll('.drop_down_pp');
+// const pastpaperDrops = document.querySelectorAll('.portfolio-item-wrapper-notes-pastpaper');
+
+// toggleArrowsPastpaper.forEach((toggleArrow_pastpaper) => {
+//     toggleArrow_pastpaper.addEventListener('click', () => {
+//         console.log("Clicked");
+//         toggleArrow_pastpaper.classList.toggle('active');
+//         pastpaperDrops.forEach((it) => {
+//             it.classList.toggle('active');
+//         });
+//     });
+// });
+
+// const toggleArrows = document.querySelectorAll('.drop_down');
+// const courseworkDrops = document.querySelectorAll('.portfolio-item-wrapper-notes-coursework');
+
+// toggleArrows.forEach((toggleArrow) => {
+//     toggleArrow.addEventListener('click', () => {
+//         console.log("Clicked");
+//         toggleArrow.classList.toggle('active');
+//         courseworkDrops.forEach((it) => {
+//             it.classList.toggle('active');
+//         });
+//     });
+// });
+
+const toggleArrowsPastpaper = document.querySelectorAll('.drop_down_pp');
+
+toggleArrowsPastpaper.forEach((toggleArrow_pastpaper) => {
+    toggleArrow_pastpaper.addEventListener('click', () => {
+        console.log("Clicked");
+        toggleArrow_pastpaper.classList.toggle('active');
+        const pastpaperDrops = toggleArrow_pastpaper.closest('.pastpaper_dropdown').querySelectorAll('.portfolio-item-wrapper-notes-pastpaper');
+        pastpaperDrops.forEach((it) => {
+            it.classList.toggle('active');
+        });
+    });
+});
+
+const toggleArrows = document.querySelectorAll('.drop_down');
+
+toggleArrows.forEach((toggleArrow) => {
+    toggleArrow.addEventListener('click', () => {
+        console.log("Clicked");
+        toggleArrow.classList.toggle('active');
+        const courseworkDrops = toggleArrow.closest('.coursework_dropdown').querySelectorAll('.portfolio-item-wrapper-notes-coursework');
+        courseworkDrops.forEach((it) => {
+            it.classList.toggle('active');
+        });
+    });
+});
+
