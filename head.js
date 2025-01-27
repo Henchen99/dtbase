5229bf13e5b7a2336c89376743160222560c4bef
Will12Stevo <willstevens2000@hotmail.co.uk>
2025-01-27 15:03:59 +0000

    head.js implemented

--- head.js
+++ head.js
@@ -1,4 +1,8 @@
-// Dynamically inject <head> content
+// Get the current script's directory path dynamically
+const scriptPath = document.currentScript.src;
+const basePath = scriptPath.substring(0, scriptPath.lastIndexOf("/") + 1);
+
+// Dynamically inject the <head> content with proper paths
 document.head.innerHTML = `
     <title>DTBASE | Design and Technology Revision - Home page.</title>
     <meta charset="UTF-8">
@@ -6,8 +10,8 @@ document.head.innerHTML = `
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Revise and learn GCSE/A-Level material for DT with DTBase. 
     Find high quality notes, past paper questions, solutions, coursework examples and much more!">
-    <link rel="shortcut icon" type="image/x-icon" href="Favicon.png">
-    <link rel="stylesheet" href="styles.css"/>
+    <link rel="shortcut icon" type="image/x-icon" href="${basePath}Favicon.png">
+    <link rel="stylesheet" href="${basePath}styles.css">
     <!-- Start CookieYes Banner -->
     <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/bf66d40c1470575f392ed5d8/script.js"></script>
     <!-- End CookieYes Banner -->
