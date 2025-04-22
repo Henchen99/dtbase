a544a3ce109cf606f21e28eb4f897deba77ba642
Will12Stevo <willstevens2000@hotmail.co.uk>
2025-04-22 10:44:36 +0100

    change

--- script.js
+++ script.js
@@ -606,7 +606,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Add the message
     floatingNote.innerHTML = `
-        <p><strong>Hey User!</strong> We’re planning affordable online group revision sessions for upcoming A-Level/GCSE exams - pay what you can (suggested donation: £5 per session). Interested? Email us @  
+        <p><strong>Hey User!</strong> We’re planning affordable online group revision sessions for upcoming A-Level/GCSE exams - pay what you can (suggested amount: £5 per session). Interested? Email us @  
     <a href="mailto:designtechbase@gmail.com">designtechbase@gmail.com</a> to book your place.</p>
     `;
 
