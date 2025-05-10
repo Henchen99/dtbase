1253ff0faa3aa391b1bfeb99540897a713d3c3e1
Will12Stevo <willstevens2000@hotmail.co.uk>
2025-05-10 13:22:24 +0100

    Search baar

--- scripts/universal_searchenginequestions.js
+++ scripts/universal_searchenginequestions.js
@@ -496,12 +496,6 @@ const aqa_alevel_questions = [
     { question: "Materials and their applications (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A1/3.1.1_materials_and_their_applications.pdf" },
     { question: "Classification of materials (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A1/3.1.1_classification_of_materials.pdf" },
     { question: "Methods for investigating and testing materials (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A1/3.1.1_methods_for_investigating_and_testing_materials.pdf" },
-    //3.1.5 The use of finishes
-    { question: "Paper and board finishing (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A5/3.1.5_paper_and_board_finishing.pdf" },
-    { question: "Paper and board printing processes (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A5/3.1.5_paper_and_board_printing_processes.pdf" },
-    { question: "Polymer finishing (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A5/3.1.5_polymer_finishing.pdf" },
-    { question: "Metal finishing (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A5/3.1.5_metal_finishing.pdf" },
-    { question: "Wood finishing (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A5/3.1.5_wood_finishing.pdf" },
     //3.1.2 Performance characteristics of materials
     { question: "Performance characteristics of papers and boards (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A2/3.1.2_papers_and_boards.pdf" },
     { question: "Performance characteristics of polymer based sheet and film (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A2/3.1.2_polymer_based_sheet_and_film.pdf" },
@@ -513,11 +507,53 @@ const aqa_alevel_questions = [
     { question: "Composites (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A2/3.1.2_composites.pdf" },
     { question: "Smart materials (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A2/3.1.2_smart_materials.pdf" },
     { question: "Modern materials (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A2/3.1.2_modern_materials.pdf" },
+    //3.1.3 Enhancement of materials
+    { question: "Polymer enhancement (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A3/3.1.3_polymer_enhancement.pdf" },
+    { question: "Wood enhancement (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A3/3.1.3_wood_enhancement.pdf" },
+    { question: "Metal enhancement  (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A3/3.1.3_metal_enhancement.pdf" },
+    //3.1.4 Forming, redistribution and addition processes
+    { question: "Paper and board forming processes (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A4/3.1.4_paper_and_board_forming_processes.pdf" },
+    { question: "Polymer processes (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A4/3.1.4_polymer_processes.pdf" },
+    { question: "Metal processes (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A4/3.1.4_metal_processes.pdf" },
+    { question: "Wood processes (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A4/3.1.4_wood_processes.pdf" },
+    { question: "The use of adhesives and fixings (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A4/3.1.4_the_use_of_adhesives_and_fixings.pdf" },
+    { question: "Jigs and fixtures (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A4/3.1.4_jigs_and_fixtures.pdf" },
+    //3.1.5 The use of finishes
+    { question: "Paper and board finishing (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A5/3.1.5_paper_and_board_finishing.pdf" },
+    { question: "Paper and board printing processes (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A5/3.1.5_paper_and_board_printing_processes.pdf" },
+    { question: "Polymer finishing (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A5/3.1.5_polymer_finishing.pdf" },
+    { question: "Metal finishing (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A5/3.1.5_metal_finishing.pdf" },
+    { question: "Wood finishing (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A5/3.1.5_wood_finishing.pdf" },
+    //3.1.6 Modern industrial and commercial practice
+    { question: "Scales of production (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A6/3.1.6_scales_of_production.pdf" },
+    { question: "Efficient use of materials (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A6/3.1.6_efficient_use_of_materials.pdf" },
+    { question: "The use of computer systems (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A6/3.1.6_the_use_of_computer_systems.pdf" },
+    { question: "Sub-assembly (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A6/3.1.6_sub_assembly.pdf" },
+    //3.1.7 Digital design and manufacture
+    { question: "Computer aided design (CAD) (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A7/.pdf" },
+    { question: "Computer aided manufacture (CAM) (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A7/.pdf" },
+    { question: "Virtual modelling (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A7/.pdf" },
+    { question: "Rapid prototyping processes (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A7/.pdf" },
+    { question: "Electronic data interchange (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A7/.pdf" },
+    { question: "Production, planning and control (PPC) networking (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A7/.pdf" },
+    //3.1.8 The requirements for product design and development
+    { question: "Product development and improvement (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A8/3.1.8_product_development_and_improvement.pdf" },
+    { question: "Inclusive design (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A8/3.1.8_inclusive_design.pdf" },
+    //3.1.9 Health and safety
+    { question: "Safe working practices (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A9/3.1.9_safe_working_practices.pdf" },
+    { question: "Safety in products and services to the custome (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A9/3.1.9_safety_in_products_and_services_to_the_customer.pdf" },
+    //3.1.10 Protecting designs and intellectual property
+    { question: "Protecting designs and intellectual property (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A10/3.1.10_protecting_designs_and_intellectual_property.pdf" }, 
     //3.1.11 Design for manufacturing, maintenance, repair and disposal
     { question: "Manufacture, repair, maintenance and disposal (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A11/3.1.11_manufacture_repair_maintenance_and_disposal.pdf" },
     { question: "Ease of manufacture (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A11/3.1.11_ease_of_manufacture.pdf" },
     { question: "Disassembly (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A11/3.1.11_disassembly.pdf" },
-
+    //3.1.12 Feasibility studies
+    { question: "Feasibility studies (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A12/3.1.12_feasibility_studies.pdf" },
+    //3.1.13 Enterprise and marketing in the development of products
+    { question: "Enterprise and marketing in the development of products (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A13/3.1.13_enterprise_and_marketing_in_the_development_of_products.pdf" },
+    //3.1.14 Design communication
+    { question: "Design communication (AQA A-Level)", url: "/Questions (Word docs)/AQA A-Level Questions/Technical Principles/Topic A14/3.1.14_design_communication.pdf" },
     
 ];
 
